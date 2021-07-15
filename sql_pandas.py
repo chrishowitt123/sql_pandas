db2de95265581af7be9bea2866d4d754a953120f
Chris Howitt <62956122+chrishowitt123@users.noreply.github.com>
2021-07-15 20:35:02 +0100

    Update sql_pandas.py

--- sql_pandas.py
+++ sql_pandas.py
@@ -4,7 +4,17 @@ import pandas as pd
 conn = psycopg2.connect("dbname=dvdrental user=postgres password=2215")
 cur = conn.cursor()
 
-sql = ("""SELECT * from payment""")
+sql = ("""
+
+
+SELECT customer_id, sum(amount)
+FROM payment
+GROUP BY customer_id
+ORDER BY customer_id
+
+
+
+""")
     
 df = pd.read_sql_query(sql, conn)
 conn = None
